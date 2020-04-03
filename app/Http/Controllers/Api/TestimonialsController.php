202482f2e57f51a10ba42d55266b1687b3c3fca6
k2gsalem <k2gsalem@gmail.com>
2020-04-04 00:12:02 +0530

    testimonials

---
+++ app/Http/Controllers/Api/TestimonialsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Testimonials;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TestimonialsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Testimonials  $testimonials
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Testimonials $testimonials)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Testimonials  $testimonials
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Testimonials $testimonials)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Testimonials  $testimonials
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Testimonials $testimonials)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Testimonials  $testimonials
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Testimonials $testimonials)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_04_03_231158_create_testimonials_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestimonialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('testimonial_name');
+            $table->string('testimonial_title');
+            $table->mediumText('testimonial_desc')->nullable();
+            $table->string('testimonial_image');
+            $table->date('testimonial_date');
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->unsignedBigInteger('updated_by');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+}
